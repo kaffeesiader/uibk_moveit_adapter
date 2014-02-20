235894b50d6a37e6f6dd382250a72a6bd9cc8c10
Martin Griesser <kaffeesiader@hotmail.com>
2014-02-20 10:04:42 +0100

    Created new plan execution node based on move_group interface

--- src/PlanExecutionNode.cpp
+++ src/PlanExecutionNode.cpp
@@ -10,229 +10,258 @@
 #include <definitions/TrajectoryExecution.h>
 #include <definitions/TrajectoryPlanning.h>
 
-#include <moveit_msgs/GetMotionPlan.h>
-#include <moveit_msgs/ExecuteKnownTrajectory.h>
+#include <moveit/move_group_interface/move_group.h>
 
-#include <moveit/kinematic_constraints/utils.h>
 #include <boost/shared_ptr.hpp>
 
-typedef boost::shared_ptr<moveit_msgs::MotionPlanResponse> MotionPlanPtr;
+typedef boost::shared_ptr<move_group_interface::MoveGroup> MoveGroupPtr;
+typedef boost::shared_ptr<move_group_interface::MoveGroup::Plan> MotionPlanPtr;
 
-#define MAX_TRAJ_PTS 50
-#define PLAN_ATTEMPTS 2
-#define MAX_PLAN_TIME 1
+#define DEF_PLAN_ATTEMPTS 2
+#define DEF_MAX_PLAN_TIME 10
+
+#define PLANNING_SERVICE_NAME "/trajectory_planner_srv"
+#define EXECUTION_SERVICE_NAME "/trajectory_execution_srv"
 
 using namespace definitions;
 using namespace moveit_msgs;
-using namespace kinematic_constraints;
 using namespace ros;
 using namespace std;
 
-ServiceClient planning_client;
-ServiceClient execution_client;
-
-ServiceServer planning_server;
-ServiceServer execution_server;
-// caches all planned trajectories for later execution
-vector<MotionPlanPtr> motion_plans;
-
 /**
- * Convenience method to fill in the trajectory data from the given MotionPlanResponse into
- * the given Trajectory
- *
- * @param plan
- * @param trajectory
+ * Rosnode that provides the services for planning and execution of robot motions
  */
-void MotionPlanToTrajectory(const MotionPlanResponse &plan, Trajectory &trajectory) {
-	const vector<trajectory_msgs::JointTrajectoryPoint> &points = plan.trajectory.joint_trajectory.points;
+class PlanExecution {
 
-	// pick each point from the trajectory and create a UIBKRobot object
-	for (size_t i = 0; i < points.size(); ++i) {
-		definitions::UIBKRobot robot_point;
-		robot_point.arm.joints.assign(points[i].positions.begin(), points[i].positions.end());
-		trajectory.robot_path.push_back(robot_point);
+private:
+
+	MoveGroupPtr _move_group;
+	// caches all planned trajectories for later execution
+	vector<MotionPlanPtr> _motion_plans;
+
+	ServiceServer _planning_server;
+	ServiceServer _execution_server;
+
+	bool _initialized;
+
+	/**
+	 * Convenience method to fill in the trajectory data from the given MotionPlanResponse into
+	 * the given Trajectory
+	 *
+	 * @param plan
+	 * @param trajectory
+	 */
+	void MotionPlanToTrajectory(const move_group_interface::MoveGroup::Plan &plan, Trajectory &trajectory) {
+		const vector<trajectory_msgs::JointTrajectoryPoint> &points = plan.trajectory_.joint_trajectory.points;
+
+		// pick each point from the trajectory and create a UIBKRobot object
+		for (size_t i = 0; i < points.size(); ++i) {
+			definitions::UIBKRobot robot_point;
+			robot_point.arm.joints.assign(points[i].positions.begin(), points[i].positions.end());
+			trajectory.robot_path.push_back(robot_point);
+		}
 	}
-}
-/**
- * Plan a trajectory for given pose goal and insert the trajectory into given vector of trajectories.
- * Returns true in case of success. Also store a Pointer to the motion plan for later usage.
- *
- * @param trajectories
- * @param goal
- * @return
- */
-bool planTrajectory(vector<Trajectory> &trajectories, geometry_msgs::PoseStamped &goal) {
+	/**
+	 * Plan a trajectory for given pose goal and insert the trajectory into given vector of trajectories.
+	 * Returns true in case of success. Also store a Pointer to the motion plan for later usage.
+	 *
+	 * @param trajectories
+	 * @param goal
+	 * @return
+	 */
+	bool planTrajectory( geometry_msgs::PoseStamped &goal, vector<Trajectory> &trajectories) {
 
-	GetMotionPlan mp;
-	MotionPlanRequest &mp_request = mp.request.motion_plan_request;
+		ROS_INFO("Setting pose target");
 
-	// should not be hardcoded...
-	mp_request.group_name = "rightArm";
-
-	// ensure that the frame id of our goal is set to the world reference frame
-	goal.header.frame_id = "world_link";
-
-	Constraints pose_goal = constructGoalConstraints("right_arm_7_link", goal, 0.001, 0.001);
-
-	mp_request.goal_constraints.push_back(pose_goal);
-	// play with theese values to achieve better performance and effizient motion plans...
-	mp_request.num_planning_attempts = PLAN_ATTEMPTS;
-	mp_request.allowed_planning_time = MAX_PLAN_TIME;
-
-	ROS_INFO("Calling plannig service...");
-	bool success = planning_client.call(mp);
-
-	if(success) {
-		MotionPlanResponse &mp_response = mp.response.motion_plan_response;
-		int trajSize = (int)mp_response.trajectory.joint_trajectory.points.size();
-
-		ROS_INFO("Planning completed with code %d", mp_response.error_code.val);
-		ROS_INFO("Planning took %.2fs", mp_response.planning_time);
-
-		if(trajSize > MAX_TRAJ_PTS) {
-			ROS_WARN("Computed trajectory contains to many points, so it will be dropped!");
+		if(!_move_group->setPoseTarget(goal)) {
+			ROS_ERROR("Setting pose target failed!");
 			return false;
+		}
+
+		ROS_INFO("Calling planning service");
+		move_group_interface::MoveGroup::Plan plan;
+		if(!_move_group->plan(plan)) {
+			ROS_WARN("No solution found");
+			return false;
+		}
+
+		int trajSize = (int)plan.trajectory_.joint_trajectory.points.size();
+
+		ROS_INFO("Planning successful completed in %.2fs", plan.planning_time_);
+		ROS_INFO("Trajectory contains %d points.", trajSize);
+		// store the motion plan for later usage (take index as id)
+		int index = _motion_plans.size();
+		MotionPlanPtr motion_plan_ptr(new move_group_interface::MoveGroup::Plan(plan));
+		_motion_plans.push_back(motion_plan_ptr);
+
+		Trajectory trajectory;
+		trajectory.trajectory_id = index;
+		// populate trajectory with motion plan data
+		MotionPlanToTrajectory(plan, trajectory);
+
+		trajectories.push_back(trajectory);
+		return true;
+
+	}
+	/**
+	 * Callback method for the trajectory planning service server
+	 *
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	bool PlanningServiceCB(TrajectoryPlanning::Request &request,
+							TrajectoryPlanning::Response &response) {
+
+		ROS_INFO("Received trajectory planning request");
+		ros::Time start_time = ros::Time::now();
+
+		// clear all previously cached motion plans
+		_motion_plans.clear();
+		vector<Trajectory> &trajectories = response.trajectory;
+
+		for(size_t i = 0; i < request.ordered_grasp.size(); ++i) {
+
+			geometry_msgs::PoseStamped &goal = request.ordered_grasp[i].grasp_trajectory[0].wrist_pose;
+			// ensure that the frame id is set to the world reference frame
+			goal.header.frame_id = "world_link";
+
+			if(!planTrajectory(goal, trajectories)) {
+				ROS_WARN("No trajectory found for grasp %d", (int)i);
+			}
+		}
+
+		if(trajectories.size() > 0) {
+			response.result = TrajectoryPlanning::Response::SUCCESS;
 		} else {
-			ROS_INFO("Trajectory contains %d points.", trajSize);
-			// store the motion plan for later usage
-			int index = motion_plans.size();
-			MotionPlanPtr motion_plan_ptr(new MotionPlanResponse(mp_response));
-			motion_plans.push_back(motion_plan_ptr);
+			response.result = TrajectoryPlanning::Response::NO_FEASIBLE_TRAJECTORY_FOUND;
+		}
 
-			Trajectory trajectory;
-			trajectory.trajectory_id = index;
-			// populate trajectory with motion plan data
-			MotionPlanToTrajectory(mp_response, trajectory);
+		ros::Duration duration = ros::Time::now() - start_time;
 
-			trajectories.push_back(trajectory);
+		ROS_INFO("Trajectory planning request completed");
+		ROS_INFO_STREAM("Total trajectory calculation took " << duration);
+
+		return true;
+	}
+	/**
+	 * Callback method for the trajectory execution service server.
+	 *
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	bool ExecutionServiceCB(TrajectoryExecution::Request &request,
+							TrajectoryExecution::Response &response) {
+
+		ROS_INFO("Received trajectory execution request");
+
+		if(request.trajectory.size() == 0) {
+			ROS_ERROR("No trajectory provided in execution request");
+			response.result = TrajectoryExecution::Response::OTHER_ERROR;
+			return false;
+		}
+
+		// use first of the trajectories...
+		int id = request.trajectory[0].trajectory_id;
+		// check if id is valid...
+		if(_motion_plans.size() <= (size_t)id) {
+			ROS_ERROR("No trajectory available with id '%d'", id);
+			response.result = TrajectoryExecution::Response::OTHER_ERROR;
+			return false;
+		}
+
+		move_group_interface::MoveGroup::Plan &plan = *_motion_plans[id];
+
+		ROS_INFO("Executing trajectory...");
+
+		if(!_move_group->execute(plan)) {
+			ROS_ERROR("Trajectory execution failed!");
+			response.result = TrajectoryExecution::Response::OTHER_ERROR;
+			return false;
+		}
+
+		ROS_INFO("Pose target reached");
+
+		// clear all previously cached motion plans because they are not valid any more...
+		_motion_plans.clear();
+		// everything went fine...
+		response.result = TrajectoryExecution::Response::SUCCESS;
+		return true;
+	}
+
+
+public:
+	/**
+	 * Initializes the move_group instance.
+	 * Reads various parameters from parameter server and sets
+	 * the values accordingly.
+	 * @param nh public nodehandle
+	 * @param p_nh private nodehandle, used for retrieving parameters
+	 */
+	bool initialize(NodeHandle &nh, NodeHandle &p_nh) {
+
+		if(_initialized) {
 			return true;
 		}
+		ROS_INFO("Launching plan_execution_node");
 
-	} else {
-		ROS_WARN("No solution found");
-		return false;
-	}
-}
-/**
- * Callback method for the trajectory planning service server
- *
- * @param request
- * @param response
- * @return
- */
-bool PlanningServiceCB(TrajectoryPlanning::Request &request,
-						TrajectoryPlanning::Response &response) {
-
-	ROS_INFO("Received trajectory planning request");
-	ros::Time start_time = ros::Time::now();
-
-	// clear all previously cached motion plans
-	motion_plans.clear();
-	vector<Trajectory> &trajectories = response.trajectory;
-
-	for(size_t i = 0; i < request.ordered_grasp.size(); ++i) {
-
-		geometry_msgs::PoseStamped &goal = request.ordered_grasp[i].grasp_trajectory[0].wrist_pose;
-		// ensure that the frame id is set to the world reference frame
-		goal.header.frame_id = "world_link";
-
-		if(!planTrajectory(trajectories, goal)) {
-			ROS_WARN("No trajectory found for grasp %d", (int)i);
-		}
-	}
-
-	if(trajectories.size() > 0) {
-		response.result = TrajectoryPlanning::Response::SUCCESS;
-	} else {
-		response.result = TrajectoryPlanning::Response::NO_FEASIBLE_TRAJECTORY_FOUND;
-	}
-
-	ros::Duration duration = ros::Time::now() - start_time;
-
-	ROS_INFO("Trajectory planning request completed");
-	ROS_INFO_STREAM("Total trajectory calculation took " << duration);
-
-	return true;
-}
-/**
- * Callback method for the trajectory execution service server.
- *
- * @param request
- * @param response
- * @return
- */
-bool ExecutionServiceCB(TrajectoryExecution::Request &request,
-						TrajectoryExecution::Response &response) {
-
-	ROS_INFO("Received trajectory execution request");
-
-	if(request.trajectory.size() == 0) {
-		ROS_ERROR("No trajectory provided in execution request");
-		return false;
-	}
-	// use first of the trajectories...
-	int id = request.trajectory[0].trajectory_id;
-
-	if(motion_plans.size() <= (size_t)id) {
-		ROS_ERROR("No trajectory available with id '%d'", id);
-		return false;
-	}
-
-	moveit_msgs::ExecuteKnownTrajectory execute_known_trajectory;
-
-	execute_known_trajectory.request.wait_for_execution = true;
-	execute_known_trajectory.request.trajectory = motion_plans[id]->trajectory;
-
-	ROS_INFO("Executing trajectory...");
-	bool success = execution_client.call(execute_known_trajectory);
-
-	// clear all previously cached motion plans because they are not valid any more...
-	motion_plans.clear();
-
-	if (success) {
-		MoveItErrorCodes &code = execute_known_trajectory.response.error_code;
-
-		if(code.val == MoveItErrorCodes::SUCCESS) {
-			response.result = TrajectoryExecution::Response::SUCCESS;
-			ROS_INFO("Execution finished successfully.");
-		} else {
-			response.result = TrajectoryExecution::Response::OTHER_ERROR;
-			ROS_WARN("Execution finished with error_code '%d'", code.val);
+		string group_name;
+		if(!p_nh.getParam("planning_group_name", group_name)) {
+			ROS_WARN("Paramteter 'planning_group_name' not set. Assuming default value.");
 		}
 
-	} else {
-		ROS_ERROR("Execution failed!");
+		ROS_INFO("Connecting to move_group '%s'", group_name.c_str());
+		_move_group.reset(new move_group_interface::MoveGroup(group_name));
 
-		return false;
+		int attempts = DEF_PLAN_ATTEMPTS;
+		if(!p_nh.getParam("num_planning_attempts", attempts)) {
+			ROS_WARN("Parameter 'num_planning_attempts' not set. Using default value instead.");
+		}
+		_move_group->setNumPlanningAttempts(attempts);
+
+		double max_plan_time = DEF_MAX_PLAN_TIME;
+		if(!p_nh.getParam("max_planning_time", max_plan_time)) {
+			ROS_WARN("Parameter 'max_planning_time' not set. Using default value instead.");
+		}
+		_move_group->setPlanningTime(max_plan_time);
+
+		string planner_id;
+		if(!p_nh.getParam("planner_name", planner_id)) {
+			ROS_WARN("Parameter 'planner_id' not set. Using default value instead.");
+		}
+		ROS_INFO("Using planner '%s'", planner_id.c_str());
+		_move_group->setPlannerId(planner_id);
+
+		ROS_INFO("Starting services...");
+
+		// start service endpoints. Use public nodehandle for that
+		_planning_server = nh.advertiseService(PLANNING_SERVICE_NAME, &PlanExecution::PlanningServiceCB, this);
+		_execution_server = nh.advertiseService(EXECUTION_SERVICE_NAME, &PlanExecution::ExecutionServiceCB, this);
+
+		ROS_INFO("Connected!");
+
+		return true;
 	}
 
-	return true;
-}
+};
 
 int main(int argc, char **argv) {
 	ros::init(argc, argv, "plan_execution_node");
+
+	ROS_INFO("Launching plan_execution_node");
+	// public nodehandle
 	ros::NodeHandle nh;
+	// private nodehandle for retrieving parameters
+	ros::NodeHandle p_nh("~");
 
-	ROS_INFO("Launching plan_executrion_node");
-
-	string planning_service_name = "/plan_kinematic_path";
-	string execution_service_name = "/execute_kinematic_path";
-
-	ROS_INFO("Waiting for 'plan_kinematic_path' service node...");
-	ros::service::waitForService(planning_service_name, -1);
-	planning_client = nh.serviceClient<moveit_msgs::GetMotionPlan>(planning_service_name);
-
-	ROS_INFO("Waiting for 'execute_kinematic_path' service node...");
-	ros::service::waitForService(execution_service_name, -1);
-	execution_client = nh.serviceClient<moveit_msgs::ExecuteKnownTrajectory>(execution_service_name);
-
-	planning_server = nh.advertiseService("/trajectory_planner_srv", PlanningServiceCB);
-	execution_server = nh.advertiseService("/trajectory_execution_srv", ExecutionServiceCB);
-
-	ROS_INFO("Connected!");
+	PlanExecution pe;
+	if(!pe.initialize(nh, p_nh)) {
+		ROS_ERROR("Unable to start plan execution node!");
+		return EXIT_FAILURE;
+	}
 
 	ros::spin();
 
 	return EXIT_SUCCESS;
 }
-
